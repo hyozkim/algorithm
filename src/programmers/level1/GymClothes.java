787520978aa1840c5d6e78331d9eb6d0345d2b03
hyozkim <sa833591@gmail.com>
2020-02-01 17:21:27 +0900

    프로그래머스 Greedy 문제 업데이트

--- src/programmers/level1/GymClothes.java
+++ src/programmers/level1/GymClothes.java
@@ -4,34 +4,36 @@ package programmers.level1;
 // Programmers Lv.1
 public class GymClothes {
     public static int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
-        int cnt = 0;
+        int answer = 0;
+        int[] student = new int[n];
+        answer = n; // 빌려줄 사람이 없으면 -1
 
-        for (int i = cnt; i < lost.length; i++) {
-            if (cnt <= lost.length) {
-                for(int j=0; j<reserve.length; j++) {
-                    if (reserve[j] + 1 == lost[i] || reserve[j] - 1 == lost[i] || reserve[j] == lost[i]) {
-                        //System.out.println("lost[i] " + lost[i] + ", cnt = " + cnt);
-                        answer++;
-                        cnt++;
-                        //System.out.println("cnt = " + cnt);
-                        break;
-                    }
+        /* 내껀 내가 입게 된다. */
+        for(int l : lost )      { student[l-1] -= 1; }
+        for( int r : reserve )  { student[r-1] += 1; }
+
+        for(int i=0; i<n; i++) {
+            if( student[i] == -1 ) {    // 체육복이 없으면
+                if( i-1>=0 && student[i-1] == 1 ) { // 왼쪽 옆에 있는 친구가 빌려준다.
+                    student[i] ++;
+                    student[i-1] --;
+                } else if( i+1 < n && student[i+1] == 1 ) { // 오른쪽 옆에 있는 친구가 빌려준다.
+                    student[i] ++;
+                    student[i+1] --;
+                } else {    // 빌려줄 사람이 없으면 이친구는 체육복을 못입는다.
+                    answer--;
                 }
-            } else {
-                break;
             }
         }
-
         return answer;
     }
-
 
     public static void main(String[] args) {
         //int n = 5; int[] lost = {2,4}; int[] reserve = {1,3,5};
         //int n = 5; int[] lost = {2,4}; int[] reserve = {3};
         //int n = 3; int[] lost = {3}; int[] reserve = {1};
-        int n = 10; int[] lost = {3,9,10}; int[] reserve = {3,8,9}; // answer : 9
+        //int n = 10; int[] lost = {3,9,10}; int[] reserve = {3,8,9}; // answer : 9
+        int n = 6; int[] lost = {2,4,5}; int[] reserve = {2,3,4}; // answer : 5
 
         System.out.println(solution(n,lost,reserve));
     }
---
+++ src/programmers/level2/LifeBoat.java
@@ -0,0 +1,27 @@
+package programmers.level2;
+
+import java.util.Arrays;
+
+public class LifeBoat {
+    public static int solution(int[] people, int limit) {
+        int n = people.length;
+
+        Arrays.sort(people);
+        int i = 0, j = n-1;
+        for(; i<j; j-- ) {
+            if( people[i] + people[j] <= limit ) {
+                i++;
+            }
+        }
+        return n-i;
+    }
+
+    public static void main(String[] args) {
+        int[] people = {70,50,80,50}; int limit = 100;
+        //int[] people = {70,50,80}; int limit = 100;
+        //int[] people = {50,70,50,40}; int limit = 200;
+
+
+        System.out.println(solution(people,limit));
+    }
+}
