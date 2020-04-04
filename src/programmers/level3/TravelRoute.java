45739f70f69cea73a0a3a83f8828a9d8cbbd1277
hyozkim <sa833591@gmail.com>
2020-04-04 10:08:21 +0900

    2020-04-04 알고리즘 스터디 문제 재업로드 - 여행루트

--- src/programmers/level3/TravelRoute.java
+++ src/programmers/level3/TravelRoute.java
@@ -3,25 +3,24 @@ package programmers.level3;
 import java.util.*;
 public class TravelRoute {
     static ArrayList<String> list = new ArrayList<>();
-    static String route = "";
 
-    public static void dfs(String[][] tickets, boolean[] visit, String end, int cnt) {
-        route += end+",";
+    public static void dfs(String[][] tickets, StringBuilder route, boolean[] visit, String end, int cnt) {
+        route.append(end+",");
 
         // System.out.println("cnt = " + cnt + ",tickets.length="+ tickets.length);
         if(cnt == tickets.length) {
-            System.out.println(route);
-            list.add(route);
+            //System.out.println(route.toString());
+            list.add(route.toString());
             return ;
         }
 
         for(int i=0; i<tickets.length; i++) {
             if( !visit[i] && end.equals(tickets[i][0]) ) {
                 visit[i] = true;
-                dfs(tickets,visit,tickets[i][1],cnt+1);
+                dfs(tickets,route,visit,tickets[i][1],cnt+1);
 
                 visit[i] = false;
-                route = route.substring(0, route.length()-4);
+                route.setLength(route.length()-4);
             }
         }
 
@@ -33,16 +32,16 @@ public class TravelRoute {
         // route += "ICN,";
         for(int i=0; i<n; i++) {
             boolean [] visit = new boolean[n];
+            StringBuilder route = new StringBuilder();
 
             if( !visit[i] && tickets[i][0].equals("ICN") ) {
                 visit[i] = true;
-                route = tickets[i][0] + ",";
-                dfs(tickets,visit,tickets[i][1],1);
+                route.append(tickets[i][0] + ",");
+                dfs(tickets,route,visit,tickets[i][1],1);
             }
         }
 
         Collections.sort(list);
-
         return list.get(0).split(",");
     }
 
